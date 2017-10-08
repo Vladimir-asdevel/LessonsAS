85c09525a0f9d839ad96eee1f32c78a1211b8c63
Vladimir Martynov <Vladimir.asdevel@gmail.com>
2017-10-08 13:31:09 +0400

    Создан новый проект TestProject Xcode 8.2.1

---
+++ TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by mvv on 08.10.2017.
+//  Copyright © 2017 MVV Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
