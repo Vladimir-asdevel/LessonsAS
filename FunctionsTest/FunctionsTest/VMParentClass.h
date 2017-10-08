bbffde5126393bd6071b091d4d03597610409535
Vladimir Martynov <Vladimir.asdevel@gmail.com>
2017-10-08 21:21:50 +0400

    Новый класс VMParentClass

---
+++ FunctionsTest/FunctionsTest/VMParentClass.h
@@ -0,0 +1,13 @@
+//
+//  VMParentClass.h
+//  FunctionsTest
+//
+//  Created by mvv on 08.10.2017.
+//  Copyright © 2017 MVV Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VMParentClass : NSObject
+
+@end
---
+++ FunctionsTest/FunctionsTest/VMParentClass.m
@@ -0,0 +1,13 @@
+//
+//  VMParentClass.m
+//  FunctionsTest
+//
+//  Created by mvv on 08.10.2017.
+//  Copyright © 2017 MVV Corporation. All rights reserved.
+//
+
+#import "VMParentClass.h"
+
+@implementation VMParentClass
+
+@end
