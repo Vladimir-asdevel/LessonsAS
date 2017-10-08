32ef83867ffd41fc6338cb94783079540f546cd6
Vladimir Martynov <Vladimir.asdevel@gmail.com>
2017-10-08 20:27:30 +0400

    Создан новый проект FunctionsTest

---
+++ FunctionsTest/FunctionsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunctionsTest
+//
+//  Created by mvv on 08.10.2017.
+//  Copyright © 2017 MVV Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FunctionsTest/FunctionsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FunctionsTest
+//
+//  Created by mvv on 08.10.2017.
+//  Copyright © 2017 MVV Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
