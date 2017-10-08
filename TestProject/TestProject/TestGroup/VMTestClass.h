febe79b9664365137396ce92b76a2e524ada5ad0
Vladimir Martynov <Vladimir.asdevel@gmail.com>
2017-10-08 14:10:58 +0400

    Новый префикс классов VM и новый класс VMTestClass На storyboard созданы две кнопки и отображение картинок

---
+++ TestProject/TestProject/TestGroup/VMTestClass.h
@@ -0,0 +1,13 @@
+//
+//  VMTestClass.h
+//  TestProject
+//
+//  Created by mvv on 08.10.2017.
+//  Copyright © 2017 MVV Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VMTestClass : NSObject
+
+@end
---
+++ TestProject/TestProject/TestGroup/VMTestClass.m
@@ -0,0 +1,13 @@
+//
+//  VMTestClass.m
+//  TestProject
+//
+//  Created by mvv on 08.10.2017.
+//  Copyright © 2017 MVV Corporation. All rights reserved.
+//
+
+#import "VMTestClass.h"
+
+@implementation VMTestClass
+
+@end
